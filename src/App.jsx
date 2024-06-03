a775e3fa48896d2223020d186437e11f303fc653
NedmanUwU <96993132+matiiilda@users.noreply.github.com>
2024-06-03 12:34:49 +0200

    testade tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import React, { useEffect, useState } from 'react';
 import "./App.css";
+import Test from "./components/test";
 
 function App() {
 
   return (
-    <>
+    <div>
     <h1 className="text-3xl font-bold">Hello world!</h1>;
     <h2>WAZZZAPPP!</h2>
-    </>
+    <Test />
+    </div>
 
   )
 
