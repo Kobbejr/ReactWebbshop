4994d93d5f314fe222382c27a16394f1df9b0fc9
NedmanUwU <96993132+matiiilda@users.noreply.github.com>
2024-06-03 11:15:59 +0200

    Tog bort logo import från app och la till h2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <h1 className="text-3xl font-bold">Hello world!</h1>;
+  return (
+    <>
+    <h1 className="text-3xl font-bold">Hello world!</h1>;
+    <h2>WAZZZAPPP!</h2>
+    </>
+
+  )
+
 }
 
 export default App;
