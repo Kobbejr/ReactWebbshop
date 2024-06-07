2d2fc0ab70c485f88abfa18643b5e2895549ee31
nurgultekin <92712594+nurgultekin@users.noreply.github.com>
2024-06-07 12:48:36 +0200

    Created a shopping cart indicator icon which is not dynamic yet. It will later show increment and decrement of the items in the shopping cart.

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,5 +1,13 @@
-export default function test() {
-    return (
+import React from 'react';
+import CartIcon from './ShoppingFlow/CartButton';
+
+export default function Test() {
+  return (
+    <div>
+      <nav>
+        <CartIcon />
+      </nav>
+      
       <div className="relative overflow-hidden bg-white">
         <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
           <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
@@ -91,5 +99,6 @@ export default function test() {
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  );
+}
