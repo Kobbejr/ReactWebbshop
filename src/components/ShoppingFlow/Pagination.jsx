a71c2f216684575b06dddecf08d3b513f1f4ab09
NedmanUwU <96993132+matiiilda@users.noreply.github.com>
2024-06-12 13:50:30 +0200

    Lagt till filter

---
+++ src/components/ShoppingFlow/Pagination.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
+
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  const pages = [...Array(totalPages).keys()].map(num => num + 1);
+
+  return (
+    <div className="flex items-center justify-between border-t border-stone-800 px-4 py-3 sm:px-6">
+      {/* Mobile view pagination with 'Previous' and 'Next' buttons */}
+      <div className="flex flex-1 justify-between sm:hidden">
+        <button
+          onClick={() => onPageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+          className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+        >
+          Previous
+        </button>
+        <button
+          onClick={() => onPageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+          className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+        >
+          Next
+        </button>
+      </div>
+
+      {/* Desktop view pagination with detailed controls */}
+      <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+        <div>
+          <p className="text-sm text-gray-700">
+            Showing <span className="font-medium">{(currentPage - 1) * 12 + 1}</span> to <span className="font-medium">{Math.min(currentPage * 12, totalPages * 10)}</span> of <span className="font-medium">{totalPages * 10}</span> results
+          </p>
+        </div>
+        <div>
+          <nav className="bg-sky-50 isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+            <button
+              onClick={() => onPageChange(currentPage - 1)}
+              disabled={currentPage === 1}
+              className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >
+              <span className="sr-only">Previous</span>
+              <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
+            {pages.map((page) => (
+              <button
+                key={page}
+                onClick={() => onPageChange(page)}
+                className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold ${page === currentPage ? 'bg-teal-600 text-white' : 'text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50'} focus:z-20 focus:outline-offset-0`}
+              >
+                {page}
+              </button>
+            ))}
+            <button
+              onClick={() => onPageChange(currentPage + 1)}
+              disabled={currentPage === totalPages}
+              className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >
+              <span className="sr-only">Next</span>
+              <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
